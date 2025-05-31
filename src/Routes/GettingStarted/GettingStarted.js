4e4fad547cfe9ed8aac06d5517b154b102c0ffbf
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-05-31 13:12:25 -0400

    Update GettingStarted.js

--- src/Routes/GettingStarted/GettingStarted.js
+++ src/Routes/GettingStarted/GettingStarted.js
@@ -3,19 +3,17 @@
 // Just be sure to put @START, @END, and @[Spacer Name] sigils in the right places.
 // Otherwise, changes made from CLI and GUI tools may break this file.
 
-import { GetArticle } from '../../Robi/Robi.js'
 import { Row } from '../../Robi/RobiUI.js'
 
 // @START-${name}
 export default async function GettingStarted({ parent }) {
     // @START-Rows
     Row(async (parent) => {
-        const md = await GetArticle({
-            route: 'GettingStarted',
-            name: 'Test'
-        });
-        
-        parent.append(md);
+        const component = /*html*/ `
+            <div>Test</div>
+        `;
+
+        parent.append(component);
     }, { parent });
     // @END-Rows
 }
